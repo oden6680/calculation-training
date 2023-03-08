bdfa684ef5d7b2d457b546dc501e818e730e5253
oden <oden6680@gmail.com>
2023-03-08 15:34:33 +0900

    印刷以外のとりあえずの機能

--- src/app/components/factorization.jsx
+++ src/app/components/factorization.jsx
@@ -1,4 +1,7 @@
 "use client";
+
+import { InlineMath, BlockMath } from 'react-katex';
+import 'katex/dist/katex.min.css';
 import Grid from '@mui/material/Grid';
 
 const N = 25;
@@ -6,11 +9,14 @@ const minNum = -10;
 const maxNum = 10;
 
 const makeFactorizationQuestion = (num1, num2) => {
-    const xSquared = "x^2";
-    const xTerm = num1 + num2 >= 0 ? "+" + (num1 + num2) + "x" : (num1 + num2) + "x";
-    const constantTerm = num1 === 0 || num2 === 0 ? "" : num1 * num2 >= 0 ? "+" + num1 * num2 : num1 * num2;
-    return xSquared + xTerm + constantTerm;
-}
+  const xSquared = "x^2";
+  const xTerm = num1 + num2 !== 0
+    ? (num1 + num2 === 1 ? "+x" : num1 + num2 === -1 ? "-x" : (num1 + num2 > 0 ? "+" : "") + (num1 + num2) + "x")
+    : "";
+  const constantTerm = num1 === 0 || num2 === 0 ? "" : num1 * num2 >= 0 ? "+" + num1 * num2 : num1 * num2;
+  return xSquared + xTerm + constantTerm + "=";
+};
+
 
 const makeFactorizationAnswer = (num1, num2) => {
     let firstFactor = num1 === 0 ? "x" : "(x" + (num1 > 0 ? "+" + num1 : num1) + ")";
@@ -18,6 +24,9 @@ const makeFactorizationAnswer = (num1, num2) => {
     if (secondFactor === "x") {
         secondFactor = firstFactor;
         firstFactor = "x";
+    }
+    if (num1 === num2) {
+        return firstFactor + "^2";
     }
     return firstFactor + secondFactor;
 };
@@ -38,13 +47,19 @@ export const Factorization = () => {
     return (
         <Grid container>
             <Grid item xs={6}>
+                <p style={{fontSize:"3em"}}>【問題】</p>
                 {questionList.map((question,index) => (
-                    <p style={{ fontSize: "3em" }}>{ "(" + String(Number(index)+1) + ")" + question}</p>
+                    <InlineMath key={question}>
+                        { "\\Huge(" + String(Number(index)+1) + ")" + question +"\\\\"}
+                    </InlineMath>
                 ))}
             </Grid>
             <Grid item xs={6}>
+                <p style={{fontSize:"3em"}}>【解答】</p>
                 {answerList.map((answer,index) => (
-                    <p style={{ fontSize: "3em" }}>{ "(" + String(Number(index)+1) + ")" + answer}</p>
+                    <InlineMath key={question}>
+                        { "\\Huge(" + String(Number(index)+1) + ")" + answer +"\\\\"}
+                    </InlineMath>
                 ))}
             </Grid>
         </Grid>
